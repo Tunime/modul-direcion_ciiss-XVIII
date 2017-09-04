cf801a6620670a3f5fff5d1ba7f1f0b200a195b6
AlvaroMoCa <almaseb_123@hotmail.com>
2017-09-04 11:49:28 -0500

    Agregando stylos

---
+++ sass/componentes.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'direccion': {
+    'fontFamily': ''Open Sans', sans-serif',
+    'display': 'flex',
+    'flexDirection': 'column',
+    'justifyContent': 'center',
+    'alignItems': 'center',
+    'fontSize': [{ 'unit': 'px', 'value': 14 }],
+    'height': [{ 'unit': 'vh', 'value': 100 }]
+  },
+  'direccion direccion_logos img': {
+    'width': [{ 'unit': 'em', 'value': 15 }]
+  },
+  'direccion direccion_texto': {
+    'margin': [{ 'unit': 'em', 'value': 3 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
+    'fontSize': [{ 'unit': 'em', 'value': 1.2 }],
+    'display': 'flex',
+    'flexDirection': 'column',
+    'justifyContent': 'center',
+    'alignItems': 'center'
+  }
+});
--- sass/main.js
+++ sass/main.js
@@ -1,4 +1,33 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+  'import': 'url("https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i")',
+  'direccion': {
+    'display': 'flex',
+    'flexDirection': 'column',
+    'justifyContent': 'center',
+    'alignItems': 'center',
+    'fontSize': [{ 'unit': 'px', 'value': 14 }],
+    'height': [{ 'unit': 'vh', 'value': 100 }]
+  },
+  'direccion direccion_logos img': {
+    'width': [{ 'unit': 'em', 'value': 18 }]
+  },
+  'direccion direccion_texto p': {
+    'fontSize': [{ 'unit': 'em', 'value': 1.5 }]
+  },
+  '*': {
+    'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
+    'padding': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }]
+  },
+  'li': {
+    'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
+    'padding': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
+    'listStyle': 'none'
+  },
+  'a': {
+    'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
+    'padding': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
+    'textDecoration': 'none'
+  }
 });
