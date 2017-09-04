a21a66fce0c1a335df65c154bae5c128e2c60838
AlvaroMoCa <almaseb_123@hotmail.com>
2017-09-04 12:12:00 -0500

    terminado

--- sass/componentes.js
+++ sass/componentes.js
@@ -11,7 +11,10 @@ export default StyleSheet.create({
     'height': [{ 'unit': 'vh', 'value': 100 }]
   },
   'direccion direccion_logos img': {
-    'width': [{ 'unit': 'em', 'value': 15 }]
+    'width': [{ 'unit': 'em', 'value': 15 }],
+    '<w422': {
+      'width': [{ 'unit': 'em', 'value': 10 }]
+    }
   },
   'direccion direccion_texto': {
     'margin': [{ 'unit': 'em', 'value': 3 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
@@ -20,5 +23,21 @@ export default StyleSheet.create({
     'flexDirection': 'column',
     'justifyContent': 'center',
     'alignItems': 'center'
+  },
+  'direccion direccion_texto p': {
+    'textAlign': 'center'
+  },
+  'direccion direccion_iconos': {
+    'margin': [{ 'unit': 'em', 'value': 2 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }]
+  },
+  'direccion direccion_iconos a': {
+    'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'em', 'value': 0.5 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'em', 'value': 0.5 }]
+  },
+  'direccion direccion_iconos img': {
+    'width': [{ 'unit': 'em', 'value': 2.5 }]
+  },
+  'direccion direccion_iconos img:hover': {
+    'width': [{ 'unit': 'em', 'value': 2.8 }],
+    'transition': 'width 0.5s'
   }
 });
